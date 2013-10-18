9387eebbc9ecae6d776976efe44619a5f05966b0
Marco Rovere <marco.rovere@cern.ch>
2013-10-18 12:34:31 +0200

    Add my stuff and fix vi/vim

--- custom/plugins/localenv/localenv.plugin.zsh
+++ custom/plugins/localenv/localenv.plugin.zsh
@@ -1,14 +1,2 @@
-export MAGICK_HOME="/usr/local/ImageMagick"
-export PATH="$MAGICK_HOME/bin:$PATH"
-export PATH="/usr/local/texlive/2012/bin/x86_64-darwin/:/usr/local/texlive/2010/bin/x86_64-darwin/:$PATH"
-export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib"
-
-export ROOTSYS="/usr/local/root/"
-export PYTHONPATH="$ROOTSYS/lib"
-export PATH="$ROOTSYS/bin:$PATH"
-export PATH="~$USER/bin:$PATH"
-export DYLD_LIBRARY_PATH="$ROOTSYS/lib"
-
-export TEXINPUTS="./:$HOME/Documents/Presentazioni:$HOME/CERN-tex/texmf-local/tex/{latex,generic,}//:"
-
-export PYTHONSTARTUP=/Users/rovere/.pystartup
+export PYTHONSTARTUP="/afs/cern.ch/user/r/rovere/.pystartup"
+export VISUAL=`which vi`
