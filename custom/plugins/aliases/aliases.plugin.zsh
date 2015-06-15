d90b7c13fbcd72dad7dd7e20289a597547485348
Marco Rovere <marco.rovere@cern.ch>
2015-06-15 11:49:36 +0200

    Add alias to automate release notes production

--- custom/plugins/aliases/aliases.plugin.zsh
+++ custom/plugins/aliases/aliases.plugin.zsh
@@ -29,6 +29,7 @@ alias setupCrab='source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh && source /a
 
 alias t='/usr/bin/time -f "%E"'
 
+alias make_release_notes='(cd /afs/cern.ch/work/r/rovere/public/ReleaseNotes && release_notes.py)'
 hless () {highlight -A $* | less -n -r}
 eosrmdir () {
   eosrepo=$*
