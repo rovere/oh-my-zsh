90dd058056d2903012d9f6e380cbf14b9a011e50
Marco Rovere <marco.rovere@cern.ch>
2017-02-15 14:12:04 +0100

    Added source-highlight support

--- custom/plugins/localenv/localenv.plugin.zsh
+++ custom/plugins/localenv/localenv.plugin.zsh
@@ -3,6 +3,7 @@ export PATH="$MAGICK_HOME/bin:$PATH"
 export PATH="/usr/texbin:/usr/local/texlive/2015/bin/x86_64-darwin:$PATH"
 export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib"
 
+# Setup ROOT
 export ROOTSYS="/Users/rovere/root"
 export PYTHONPATH="$ROOTSYS/lib"
 export PYTHONPATH="/Library/Python/2.7/site-packages/:$PYTHONPATH"
@@ -11,5 +12,12 @@ export PATH="~$USER/bin:$PATH"
 export PATH=/usr/local/MacGPG2/bin:${PATH}
 export DYLD_LIBRARY_PATH="$ROOTSYS/lib"
 
+# Setup Go
+export PATH="/usr/local/go/bin":${PATH}
+
 export TEXINPUTS="./:$HOME/Documents/Presentazioni:$HOME/CERN-tex/texmf-local/tex/{latex,generic,}//:"
 
+# Setup source-highlight
+LESSPIPE=`which src-hilite-lesspipe.sh`
+export LESSOPEN="| ${LESSPIPE} %s"
+export LESS=' -R -X -F '
