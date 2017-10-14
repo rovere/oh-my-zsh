1437e3cfa079f6cd995bee99f88509f39876377b
Marco Rovere <marco.rovere@cern.ch>
2017-10-14 08:00:14 +0200

    Add DQMGUI7 VM

--- custom/plugins/aliases/aliases.plugin.zsh
+++ custom/plugins/aliases/aliases.plugin.zsh
@@ -30,6 +30,7 @@ alias vo22="ssh -t -XY vocms022.cern.ch"
 alias v2="ssh -t -XY vinavx2.cern.ch"
 alias o9="ssh -t -XY olsnbc09.cern.ch"
 alias m7="ssh -t -XY mrovere-slc7.cern.ch"
+alias d7="ssh -t -XY dqmgui7.cern.ch"
 
 alias work='cd /afs/cern.ch/work/r/rovere'
 alias slc5='export SCRAM_ARCH=slc5_ia32_gcc434'
