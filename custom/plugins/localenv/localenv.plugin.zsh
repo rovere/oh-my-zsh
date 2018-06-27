9aec066a704cdfb7ad946d03a99d3f903b50bf2d
Marco Rovere <marco.rovere@cern.ch>
2018-06-27 15:04:16 +0200

    Add gpg pinentry function

--- custom/plugins/localenv/localenv.plugin.zsh
+++ custom/plugins/localenv/localenv.plugin.zsh
@@ -5,3 +5,8 @@ export TEXINPUTS="./:"
 # Setup source-highlight
 export LESS='-C -M -I -j 10 -# 4 -R -F -X '
 export LESSOPEN="| $(which src-hilite-lesspipe.sh) %s"
+
+function gpgpin() {
+  echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null 2>&1
+  return $?
+}
