f67aac65a30b90d1fcf8ed5d2098cef76e6b0396
Marco Rovere <marco.rovere@cern.ch>
2019-09-30 11:33:19 +0200

    Add PR functions and patatrack02 alias

--- custom/plugins/localenv/localenv.plugin.zsh
+++ custom/plugins/localenv/localenv.plugin.zsh
@@ -14,3 +14,50 @@ function gpgpin() {
   echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null 2>&1
   return $?
 }
+
+function prdates() {
+  token=$(cat ~/.private/.github.token)
+  prjson=$(curl -H "Authorization: token ${token}" https://api.github.com/repos/cms-sw/cmssw/issues/$1 2>/dev/null)
+  title=$(echo ${prjson} | tr '\r\n' ' ' | jq '.title' |tr -d '"')
+  user=$(echo ${prjson} | tr '\r\n' ' ' | jq '.user.login')
+  d1=$(echo ${prjson} | tr '\r\n' ' ' | jq '.created_at' | tr -d '"')
+  d2=$(echo ${prjson} | tr '\r\n' ' ' | jq '.closed_at'  | tr -d '"')
+  body=$(echo ${prjson} | tr '\r\n' ' ' | jq '.body')
+  echo "PR $1 has title: $title"
+  echo "PR $1 submitted by $user"
+  echo "PR $1 description:\n ${body}"
+  echo "PR $1 created on: ${d1}"
+  echo "PR $1 closed  on: ${d2}"
+  d1=$(date -d $d1 +%s)
+  d2=$(date -d $d2 +%s)
+  days=$(( (d2 - d1) / 86400 ))
+  echo ${days} days
+  echo "\`\`${title}'' [\\\emph{${user}}, ${days} days]"
+}
+
+function openpr() {
+  token=$(cat ~/.private/.github.token)
+  prjson=$(curl -H "Authorization: token ${token}" https://api.github.com/repos/cms-sw/cmssw/issues 2>/dev/null)
+  echo "${prjson}" | sed -e 's/[\r\t]/ /g' | tr '\n' ' ' | jq '.[] | .number,.user.login,.title,.body,.labels[].name'
+}
+
+function openprlabel() {
+  token=$(cat ~/.private/.github.token)
+  prjson=$(curl -H "Authorization: token ${token}" "https://api.github.com/repos/cms-sw/cmssw/issues?labels=$1" 2>/dev/null)
+  echo "${prjson}" | sed -e 's/[\r\t]/ /g' | tr '\n' ' ' | jq '.[] | .number,.user.login,.title,.body,.labels[].name'
+}
+
+function pdf2png() {
+  in=$1
+  out=$(basename $in)
+  out=${out%.pdf} # Strip pdf from the output filename
+  convert           \
+   -verbose       \
+   -density 150   \
+   -trim          \
+    ${in}      \
+   -quality 100   \
+   -flatten       \
+   -sharpen 0x1.0 \
+    $out.png
+}
