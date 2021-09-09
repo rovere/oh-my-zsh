7520cb122bb59c035e2a876e71fa49810900eb43
Marco Rovere <marco.rovere@cern.ch>
2021-09-09 19:23:56 +0200

    Improve less default settings

--- custom/plugins/localenv/localenv.plugin.zsh
+++ custom/plugins/localenv/localenv.plugin.zsh
@@ -24,8 +24,7 @@ fi
 export TEXINPUTS="./:"
 
 # Setup source-highlight
-export LESS='-C -M -I -j 10 -# 4 -R -F -X '
-export LESSOPEN="| $(which src-hilite-lesspipe.sh) %s"
+export LESS='-C -M -I -# 4 -R -X '
 
 function gpgpin() {
   echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null 2>&1
